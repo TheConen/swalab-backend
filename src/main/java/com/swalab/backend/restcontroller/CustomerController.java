d63c6bcddd222b60b7121f236c3bf6fc81472d59
DESKTOP-AHD9183\RebeccaS <rebecca.sigmund@web.de>
2018-11-07 22:25:05 +0100

    Added Swagger for API-Documentation. (http://localhost:8080/swagger-ui.html)

--- src/main/java/com/swalab/backend/restcontroller/CustomerController.java
+++ src/main/java/com/swalab/backend/restcontroller/CustomerController.java
@@ -3,7 +3,7 @@ package com.swalab.backend.restcontroller;
 import com.swalab.backend.database.Database;
 import com.swalab.backend.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +18,8 @@ public class CustomerController {
         this.database = database;
     }
 
-    @RequestMapping("/customer/all")
-    public List<Customer> getHello() {
+    @GetMapping("/customer/all")
+    public List<Customer> getAllCustomers() {
         return database.getCustomers();
     }
 }
--- src/main/java/com/swalab/backend/restcontroller/testController.java
+++ src/main/java/com/swalab/backend/restcontroller/testController.java
@@ -1,7 +1,7 @@
 package com.swalab.backend.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +12,7 @@ public class testController {
         //ToDo
     }
     
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String getHello() {
         return "Hello World! " + Math.random();
     }
