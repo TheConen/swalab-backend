4a4a4f82d48db70b03844fc0f09e40f047b0678b
Lu <prlu1014@hs-karlsruhe.de>
2019-01-26 13:20:02 +0100

    Writing current process id into own file

--- src/main/java/com/swalab/backend/SwalabBackendApplication.java
+++ src/main/java/com/swalab/backend/SwalabBackendApplication.java
@@ -5,15 +5,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.*;
+
 @SpringBootApplication
 public class SwalabBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwalabBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        writingProcessID();
 
-	@Bean
-	public TechnicianDatabase database() {
-		return new TechnicianDatabase();
-	}
+
+        SpringApplication.run(SwalabBackendApplication.class, args);
+    }
+
+    private static void writingProcessID() {
+        long pid = ProcessHandle.current().pid();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("pid.info"))) {
+            bw.write(pid + "");
+            bw.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Bean
+    public TechnicianDatabase database() {
+        return new TechnicianDatabase();
+    }
 }
